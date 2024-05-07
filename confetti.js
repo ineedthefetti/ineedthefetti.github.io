8025848ebe438840d68c818957eb057dc644d8ca
Ray Terrill <rayterrill@Rays-MacBook-Pro.local>
2024-05-06 21:18:50 -0700

    update

---
+++ confetti.js
@@ -0,0 +1,50 @@
+var count = 200;
+var defaults = {
+    origin: { y: 0.7 }
+};
+
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioCtx = new AudioContext();
+var snd = new Audio("airhorn.mp3")
+snd.preload = 'auto';
+snd.load();
+
+function inner(particleRatio, opts) {
+    confetti({
+        ...defaults,
+        ...opts,
+        particleCount: Math.floor(count * particleRatio)
+    });
+}
+
+function fire() {
+    var click = snd.cloneNode();
+    click.play();
+
+    inner(0.25, {
+        spread: 26,
+        startVelocity: 55,
+    });
+
+    inner(0.2, {
+        spread: 60,
+    });
+
+    inner(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8
+    });
+
+    inner(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2
+    });
+
+    inner(0.1, {
+        spread: 120,
+        startVelocity: 45,
+    });
+}
